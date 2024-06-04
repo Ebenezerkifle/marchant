89c1dc7d3861ee57031219e43c9c0e38f08c41d8
Abeniezer Kifle <49494895+Ebenezerkifle@users.noreply.github.com>
2024-06-04 15:33:17 +0300

    some changes made

--- lib/models/category_model.dart
+++ lib/models/category_model.dart
@@ -1,4 +1,3 @@
-// ignore_for_file: public_member_api_docs, sort_constructors_first
 import 'dart:convert';
 
 class Category {
@@ -30,17 +29,17 @@ class Category {
     );
   }
 
-  // String toJson() => json.encode(toMap());
+  String toJson() => json.encode(toMap());
 
-  // factory Category.fromJson(String source) =>
-  //     Category.fromMap(json.decode(source) as Map<String, dynamic>);
+  factory Category.fromJson(String source) =>
+      Category.fromMap(json.decode(source) as Map<String, dynamic>);
 
-  // static List<Category> listFromApi(List<dynamic> categories) {
-  //   List<Category> list = [];
-  //   for (var ele in categories) {
-  //     Category c = Category.fromMap(ele);
-  //     list.add(c);
-  //   }
-  //   return list;
-  // }
+  static List<Category> listFromApi(List<dynamic> categories) {
+    List<Category> list = [];
+    for (var ele in categories) {
+      Category c = Category.fromMap(ele);
+      list.add(c);
+    }
+    return list;
+  }
 }
